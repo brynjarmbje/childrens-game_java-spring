c7c476a2d2cb8044175bfba4f724075a8a91860c
theTastywizard <mattipatti1998@gmail.com>
2024-10-11 13:07:21 +0000

    move LoginRequest to entity

--- src/main/java/com/login/controller/LoginController.java
+++ src/main/java/com/login/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.login.service.LoginService;
+import com.login.entity.LoginRequests;
 
 @RestController
 @RequestMapping("/auth")
