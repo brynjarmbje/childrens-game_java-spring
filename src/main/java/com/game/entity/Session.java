9b56f7cd7c38e9c7221965b2eff00d95dd86e644
TheTastyWizard <mattipatti1998@gmail.com>
2024-11-06 23:40:38 +0000

    add methods and attributes to session

--- src/main/java/com/game/entity/Session.java
+++ src/main/java/com/game/entity/Session.java
@@ -1,2 +1,12 @@
 package com.game.entity;public class Session {
+    private Long id;
+    private int type;
+    private int level;
+
+	public Session(Long id, int type, int level) {
+		this.id = id;
+		this.type = type;
+        this.level = level;
+	}
+    
 }
