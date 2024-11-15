d62bbb52b0538397ebf5455e71354da1feebba68
theTastyWizard <mattipatti1998@gmail.com>
2024-11-15 16:26:13 +0000

    comment out question making and remove .sql file for admin creation, not needed for persistant DB

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
--- this file is run when application is started but after hibernate init
-INSERT INTO admin (username, password,issupervisor)
-VALUES ('admin', 'admin',true);
-
-select * from admin;
